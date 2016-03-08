b2a837ab13a0cecd1c03b9b87274d26b9ec0b164
Edgar Castañeda <edaniel15@gmail.com>
2016-03-08 09:43:48 -0500

    chore(core): cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -45,31 +45,6 @@ else
 	notificar "[Ok] Archivo .vimrc"
 fi
 
-if [ ! -x /usr/bin/emacs ] ; then                                
-    notificar "Instalando Emacs"
-    sudo apt-get install emacs24 --yes
-else
-	notificar "[Ok] Emacs24"
-fi
-
-if [ ! -x /usr/bin/ruby ] ; then                                
-    sudo apt-get -y install python-pip
-    sudo apt-get -y install build-essential
-    sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
-    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-    ruby-dev mercurial
-else
-	notificar "[Ok] Ruby"
-fi
-
-if [ ! -x /usr/bin/svn ] ; then                                
-   sudo apt-get install subversion
-   sudo apt-get install libapache2-svn
-else
-	notificar "[Ok] Ruby"
-fi
-
 if [ ! -x /usr/bin/curl ] ; then                                
     notificar "Instalando Curl"
     sudo apt-get install curl --yes
@@ -137,71 +112,37 @@ else
 	notificar "[Ok] Tmux"
 fi
 
-if [ ! -x /usr/bin/java ] ; then   
-    notificar "Instalando Java"
-    sudo add-apt-repository ppa:webupd8team/java
-    sudo apt-get update
-    sudo apt-get install oracle-java8-installer
-else
-	notificar "[Ok] Java 8"
-fi
-
-if [ ! -d ~/netbeans-8.0.2 ]; then
-	notificar "Instalando NetBeans 8.0.2"
-	if [ ! -f ~/netbeans.sh ]; then
-		notificar "Obteniendo Basheroo"
-        wget -O ~/netbeans.sh http://download.netbeans.org/netbeans/8.0.2/final/bundles/netbeans-8.0.2-php-linux.sh
-        sudo chmod +x ~/netbeans.sh
-	bash ~/netbeans.sh
-	else
-		notificar "[Ok] Bash netbeans"
-	fi
-else
-	notificar "[Ok] netBeans8"
-fi
-
-if [ ! -d ~/.prelude ]; then
-	notificar "Instalando EmacsPrelude"
-	rm -rf .emacs.d
-	git clone git://github.com/bbatsov/prelude.git ~/.prelude
-	ln -s ~/.prelude ~/.emacs.d
-else
-	notificar "[Ok] Prelude"
-fi
-
 if [ ! -x /usr/bin/docker ] ; then
   notificar "Instalando Docker"
-  #curl -sSL https://get.docker.com/ | sh
-  #sudo usermod -aG docker $USER  
-  #sudo service docker start
+  curl -fsSL https://get.docker.com/ | sh
+  sudo usermod -aG docker $USER
+  sudo service docker start
 else
   notificar "[Ok] Docker"
 fi
 
-#if [ ! -x /usr/local/bin/docker-compose ] ; then
-  #notificar "Instalando Docker-Compose"
-  #sudo su
-  #curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-  #chmod +x /usr/local/bin/docker-compose
-  #exit
-  #docker-compose --version
-#else
-#  notificar "[Ok] docker-compose"
-#fi
+if [ ! -x /usr/local/bin/docker-compose ] ; then
+  notificar "Instalando Docker-Compose"
+  sudo curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  docker-compose --version
+else
+  notificar "[Ok] docker-compose"
+fi
 
 if [ ! -x /usr/bin/ansible ] ; then   
     notificar "Instalando Ansible"
-    sudo ifconfig | grep inet
-    sudo apt-add-repository ppa:ansible/ansible -y
-    sudo apt-get update && sudo apt-get install ansible -y
-    sudo pip install MySQL-python
+    sudo apt-get install software-properties-common
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-get update
+    sudo apt-get install ansible --yes
 else
 	notificar "[Ok] Ansible"
 fi
 
 if [ ! -x /usr/bin/vagrant ] ; then   
     notificar "Instalando Vagrant"
-	wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.4_x86_64.deb -O ~/vagrant.deb
+	wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb -O ~/vagrant.deb
 	sudo dpkg -i ~/vagrant.deb
 else
 	notificar "[Ok] Vagrant"
@@ -209,16 +150,9 @@ fi
 
 if [ ! -x /usr/bin/virtualbox ] ; then   
     notificar "Instalando VirtualBox"
-	sudo apt-get install virtualbox-4.3 --yes
+	sudo apt-get install virtualbox --yes
 else
 	notificar "[Ok] VirtualBox"
-fi
-
-if [ ! -d ~/Dev/ansible/playbook ]; then
-    notificar "Crear Carpeta PlayBook"
-    mkdir -p ~/Dev/ansible/playbook
-else
-    notificar "[Ok] Creado Carpeta PlayBook"
 fi
 
 if [ ! -d ~/.spf13-vim-3 ]; then
@@ -229,5 +163,4 @@ else
 fi
 
 notificar "[Ok] Finalizado | Enjoy! | PoweredBy: @Edux87"
-
 sudo apt-get autoremove -y
